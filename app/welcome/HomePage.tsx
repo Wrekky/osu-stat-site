b4050de7ce99c83f52a3d1a4c200294c8f07d276
Andrew Friesen <99490619+Wrekky@users.noreply.github.com>
2025-08-22 19:25:37 -0400

    added a search bar using flowbite

--- app/welcome/HomePage.tsx
+++ app/welcome/HomePage.tsx
@@ -1,17 +1,21 @@
-import { PlayGraph } from "~/graphs/PlayGraph";
-import { PpGraph } from "~/graphs/PpGraph";
-import { TagsGraph } from "~/graphs/TagsGraph";
-
+import { PlayGraph } from "~/components/graphs/PlayGraph";
+import { PpGraph } from "~/components/graphs/PpGraph";
+import { TagsGraph } from "~/components/graphs/TagsGraph";
+import { TopBar } from "~/components/TopBar";
 export function HomePage() {
   return (
     <main>
-      <div>
+      <div className="flex justify-center m-8">
+        <TopBar />
+      </div>
+      
+      <div className="flex justify-center">
         <PlayGraph />
       </div>
-      <div>
+      <div className="flex justify-center">
         <PpGraph />
       </div>
-      <div>
+      <div className="flex justify-center">
         <TagsGraph />
       </div>
     </main>
